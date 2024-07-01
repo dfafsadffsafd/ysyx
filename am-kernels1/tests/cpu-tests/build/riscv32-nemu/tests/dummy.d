fcf0631cb948fc3be79a02f17633626d52a2063b
221220000-Liu yaping <2429542895@qq.com>
2024-07-01 13:30:40 +0800

    d

--- am-kernels1/tests/cpu-tests/build/riscv32-nemu/tests/dummy.d
+++
@@ -1,2 +0,0 @@
-/home/liu/Public/ysyx-workbench/am-kernels/tests/cpu-tests/build/riscv32-nemu/tests/dummy.o: \
- /home/liu/Public/ysyx-workbench/am-kernels/tests/cpu-tests/tests/dummy.c
--- nvboard1/LAB6/build/obj_dir/auto_bind.d
+++
@@ -1,9 +0,0 @@
-auto_bind.o: \
- /home/liu/Public/ysyx-workbench/nvboard/LAB6/build/auto_bind.cpp \
- /home/liu/Public/ysyx-workbench/nvboard/usr/include/nvboard.h \
- /home/liu/Public/ysyx-workbench/nvboard/usr/include/pins.h Vtop.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_config.h \
- /usr/local/share/verilator/include/verilated_types.h \
- /usr/local/share/verilator/include/verilated_funcs.h
